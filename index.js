dadd1d62f6f0d9f1558ebb5e530fddfe9b39dba4
jkruse8848 <joseph.kruse88@gmail.com>
2022-09-19 17:47:45 -0500

    week05 capstone changes

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-/* Set the width of the sidebar to 250px (show it) */
-function openNav() {
-  document.getElementById("mySidepanel").style.width = "250px";
+// Event Listener for Hamburger Icon Menu
+// document.querySelector(".fa-bars").addEventListener("click", () => {
+//     document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+//   });
+
+//Event Listener for collapsible side panel  
+function closeNav(){
+    document.querySelector(".fa-angle-double-left").addEventListener("click", () => {
+        document.querySelector("nav > ul").classList.toggle(".mini-nav")
+    })
 }
 
-/* Set the width of the sidebar to 0 (hide it) */
-function closeNav() {
-  document.getElementById("mySidepanel").style.width = "0";
-}
+//Side Panel Navigation
+function homePage(el){
+    window.location.href='./index.html';
+  }
+
+  function aboutPage(link){
+    location.href='/about.html'
+  }
